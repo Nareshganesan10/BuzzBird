1c8686d8704a857ec2d0d18738391dbe06749e07
Nareshganesan10 <51469907+Nareshganesan10@users.noreply.github.com>
2023-06-06 23:18:59 +0530

    Delete BuzzBird directory

--- BuzzBird/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from BuzzBird import models
-
-admin.site.register(models.Post)
--- BuzzBird/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from BuzzBird import views
-
-urlpatterns = [
-    path("home/", view=views.home, name="home"),
-    path("signin/", view=views.signin, name="signin"),
-    path("signup/", view=views.signup, name="signup")
-]
